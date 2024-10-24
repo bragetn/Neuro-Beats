a58ef2fded14c647e1c264b270796f2c12300be8
bragetn <bragetn@stud.ntnu.no>
2024-10-24 09:51:12 +0200

    Add original music and sync with note spawner

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -5,13 +5,26 @@ const HitType = Radio.HitType
 @onready var left_rumbler: XRToolsRumbler = $LeftHand/LeftRumbler
 @onready var right_rumbler: XRToolsRumbler = $RightHand/RightRumbler
 
+var rumble_active: bool = false
+
 
 func _ready() -> void:
+	Radio.start.connect(start)
+	Radio.stop.connect(stop)
 	Radio.note_hit.connect(note_hit)
 
 
+func start():
+	rumble_active = true
+
+
+func stop():
+	rumble_active = false
+
+
 func note_hit(hit_type: HitType):
-	if hit_type == HitType.LEFT:
-		left_rumbler.rumble()
-	else:
-		right_rumbler.rumble()
+	if rumble_active:
+		if hit_type == HitType.LEFT:
+			left_rumbler.rumble()
+		else:
+			right_rumbler.rumble()
