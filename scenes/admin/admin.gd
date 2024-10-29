b3db27d34db1a0b09d9ea7d5ce76d59d6d553a06
bragetn <brage.naustan@gmail.com>
2024-10-29 13:46:26 +0100

    Made sliders update parameter

--- scenes/admin/admin.gd
+++ scenes/admin/admin.gd
@@ -1,5 +1,23 @@
 extends Control
 class_name Admin
 
+const GameParam = Radio.GameParam
+
 func _on_button_pressed() -> void:
 	Radio.start.emit()
+
+
+func _on_song_speed_slider_value_changed(value: float) -> void:
+	Radio.update_game_param.emit(GameParam.SONG_SPEED, value)
+
+
+func _on_note_speed_slider_value_changed(value: float) -> void:
+	Radio.update_game_param.emit(GameParam.NOTE_SPEED, value)
+
+
+func _on_beat_tempo_slider_value_changed(value: float) -> void:
+	Radio.update_game_param.emit(GameParam.BEAT_TEMPO, value)
+
+
+func _on_spawn_distance_slider_value_changed(value: float) -> void:
+	Radio.update_game_param.emit(GameParam.SPAWN_DISTANCE, value)
